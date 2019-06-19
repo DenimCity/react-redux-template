eb6e2a8d97e2ae2f2475e9e5419811c16ce0e96b
jxa8hst <jean_altidor@homdepot.com>
2019-06-19 10:06:17 -0400

    pure component

--- src/components/launches/index.js
+++ src/components/launches/index.js
@@ -1,59 +1,54 @@
 import dayjs from 'dayjs';
-import PropTypes from "prop-types";
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { connect } from "react-redux";
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 import { getLaunches } from '../../redux/actions/launches';
 
-export class Launches extends Component {
+const Launches = (props) =>  {
 
-      static propTypes = {
-            launches: PropTypes.array.isRequired,
-      }
 
-      componentWillMount(){
-            this.props.getLaunches()
-      }
+      const { launches } = props;
 
-    render() {
-          const { launches } = this.props;
+      
+      useEffect(()=> {
+            props.getLaunches()
+      });
             return (
-                  <Fragment>
-                        <h1>Launches</h1>
-                  <ReactTable 
-                        data= {launches}
-                        defaultPageSize={ 8 }
-                        resizable={ true }
-                        columns={[
-                              { 
-                              Header: 'Flight Number',
-                              Cell: props => <div style={{ textAlign: 'center'}}> {props.original.flight_number}</div>
-                              },
-                              { 
-                              Header: 'Mission',
-                              Cell: props => <div style={{ textAlign: 'center'}}> {props.original.mission_name}</div>
-                              },
-                              {
-                              Header: 'Details',
-                              Cell:  props => <div style={{ textAlign: 'center'}}> {props.original.details && props.original.details.length > 45 ? `${props.original.details.substring(0,20)}...` : 'Details N/A'}</div>
-                              },
-                              { Header: 'Launch Status',
-                              Cell: props => <div style={{ textAlign: 'center'}}> {props.original.launch_success ? "Successful" : "Failed Launch"}</div>
-                              },
-                              { Header: 'Launch Date',
-                              Cell: props => <div style={{ textAlign: 'center'}}> {dayjs(props.original.launch_date_utc).format("MM-DD-YYYY hh:mm")}</div>
-                              },
-                              { Header: 'Launch Year',
-                              Cell: props => <div style={{ textAlign: 'center'}}> {props.original.launch_year}</div>
-                              },
-                              { Header: 'Launch Site',
-                              Cell: props => <div style={{ textAlign: 'center'}}> {props.original.launch_site.site_name}</div>
-                              }
-                              ]}/>
-                 </Fragment>
-      )
-    }
+            <Fragment>
+                  <h1>Launches</h1>
+            <ReactTable 
+                  data= {launches}
+                  defaultPageSize={ 8 }
+                  resizable={ true }
+                  columns={[
+                        { 
+                        Header: 'Flight Number',
+                        Cell: props => <div style={{ textAlign: 'center'}}> {props.original.flight_number}</div>
+                        },
+                        { 
+                        Header: 'Mission',
+                        Cell: props => <div style={{ textAlign: 'center'}}> {props.original.mission_name}</div>
+                        },
+                        {
+                        Header: 'Details',
+                        Cell:  props => <div style={{ textAlign: 'center'}}> {props.original.details && props.original.details.length > 45 ? `${props.original.details.substring(0,20)}...` : 'Details N/A'}</div>
+                        },
+                        { Header: 'Launch Status',
+                        Cell: props => <div style={{ textAlign: 'center'}}> {props.original.launch_success ? "Successful" : "Failed Launch"}</div>
+                        },
+                        { Header: 'Launch Date',
+                        Cell: props => <div style={{ textAlign: 'center'}}> {dayjs(props.original.launch_date_utc).format("MM-DD-YYYY hh:mm")}</div>
+                        },
+                        { Header: 'Launch Year',
+                        Cell: props => <div style={{ textAlign: 'center'}}> {props.original.launch_year}</div>
+                        },
+                        { Header: 'Launch Site',
+                        Cell: props => <div style={{ textAlign: 'center'}}> {props.original.launch_site.site_name}</div>
+                        }
+                        ]}/>
+            </Fragment>
+            )
 }
 
 const mapStateTpProps = state => {
