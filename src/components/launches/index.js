a93833dfeb05a9028e76ba9a39a85ebf8d691ec1
jxa8hst <jean_altidor@homdepot.com>
2019-06-18 21:19:00 -0400

    component connected to the state and retrieving lauches from redux

--- src/components/launches/index.js
+++ src/components/launches/index.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import dayjs from 'dayjs';
+import PropTypes from "prop-types";
+import React, { Component, Fragment } from 'react';
+import { Table } from 'react-bootstrap';
+import { connect } from "react-redux";
+import { getLaunches } from '../../actions/launches';
 
-const Launches = () => {
-      return (
-            <div>
-                  <h1>Hi from Launches</h1>
-            </div>
+export class Launches extends Component {
+
+      static propTypes = {
+            launches: PropTypes.array.isRequired,
+      }
+
+      componentWillMount(){
+            this.props.getLaunches()
+      }
+      
+    render() {
+          const { launches } = this.props;
+            return (
+            <Fragment>
+                  <h1>Launches</h1>
+                  <Table striped bordered hover>
+                        <thead>
+                        <tr>
+                              <td>Flight Number</td>
+                              <td>Mission</td>
+                              <td>Details</td>
+                              <td>Launch Status</td>
+                              <td>Launch Date</td>
+                              <td>Launch Year</td>
+                              <td>Launch Site</td>
+                              
+                        </tr>
+                        </thead>
+                          <tbody>
+                              {launches.map((launch, index) => {
+                                    return (
+                                         <tr key={index}>
+                                               <td>{launch.flight_number}</td>
+                                               <td>{launch.mission_name}</td>
+                                               <td>{launch.details ? launch.details : 'Details N/A'}</td>
+                                               <td>{launch.launch_success ? "Successful" : "Failed Launch"}</td>
+                                               <td>{dayjs(launch.launch_date_utc).format("MM-DD-YYYY hh:mm")}</td>
+                                               <td>{launch.launch_year}</td>
+                                               <td>{launch.launch_site.site_name}</td>
+                                         </tr> 
+                                    )
+                              })}
+                        </tbody>
+            </Table>
+            </Fragment>
       )
+    }
 }
 
-export default Launches
+const mapStateTpProps = state => {
+      return {
+            launches: state.launches.launches
+      }
+}
+
+export default connect(mapStateTpProps,{ getLaunches })(Launches);
