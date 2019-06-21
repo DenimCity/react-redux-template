bca66f2a9b2c0d016876c1ad65387b60b6bafc1f
jxa8hst <jean_altidor@homdepot.com>
2019-06-21 11:01:54 -0400

    linting fixed

--- src/components/launches/index.js
+++ src/components/launches/index.js
@@ -1,60 +1,76 @@
 import dayjs from 'dayjs';
 import React, { Fragment, useEffect } from 'react';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 import { getLaunches } from '../../redux/actions/launches';
 
-const Launches = (props) =>  {
+const Launches = (props) => {
+  const { launches } = props;
 
 
-      const { launches } = props;
+  useEffect(() => {
+    props.getLaunches();
+  }, []);
 
-      
-      useEffect(()=> {
-            props.getLaunches()
-      });
-            return (
-            <Fragment>
-                  <h1>Launches</h1>
-            <ReactTable 
-                  data= {launches}
-                  defaultPageSize={ 8 }
-                  resizable={ true }
-                  columns={[
-                        { 
-                        Header: 'Flight Number',
-                        Cell: props => <div style={{ textAlign: 'center'}}> {props.original.flight_number}</div>
-                        },
-                        { 
-                        Header: 'Mission',
-                        Cell: props => <div style={{ textAlign: 'center'}}> {props.original.mission_name}</div>
-                        },
-                        {
-                        Header: 'Details',
-                        Cell:  props => <div style={{ textAlign: 'center'}}> {props.original.details && props.original.details.length > 45 ? `${props.original.details.substring(0,20)}...` : 'Details N/A'}</div>
-                        },
-                        { Header: 'Launch Status',
-                        Cell: props => <div style={{ textAlign: 'center'}}> {props.original.launch_success ? "Successful" : "Failed Launch"}</div>
-                        },
-                        { Header: 'Launch Date',
-                        Cell: props => <div style={{ textAlign: 'center'}}> {dayjs(props.original.launch_date_utc).format("MM-DD-YYYY hh:mm")}</div>
-                        },
-                        { Header: 'Launch Year',
-                        Cell: props => <div style={{ textAlign: 'center'}}> {props.original.launch_year}</div>
-                        },
-                        { Header: 'Launch Site',
-                        Cell: props => <div style={{ textAlign: 'center'}}> {props.original.launch_site.site_name}</div>
-                        }
-                        ]}/>
-            </Fragment>
-            )
-}
 
-const mapStateTpProps = state => {
-      return {
-            launches: state.launches.launches
-      }
-}
+  const tableData = launches.map(launch => ({
+    flightNumber: launch.flight_number,
+    missionName: launch.mission_name,
+    details: launch.details ? `${launch.details.substring(0, 20)}...` : 'Details N/A',
+    launchDate: dayjs(launch.launch_date_utc).format('hh:mm '),
+    launchStatus: launch.launch_success ? 'Successful' : 'Failed Launch',
+    launchYr: launch.launch_year,
+    launchSite: launch.launch_site.site_name_long,
+  }));
 
-export default connect(mapStateTpProps,{ getLaunches })(Launches);
+
+  return (
+    <Fragment>
+      <h1>Launches</h1>
+      <ReactTable
+        data={tableData}
+        defaultPageSize={8}
+        resizable
+        columns={[
+          {
+            Header: 'Flight Number',
+            accessor: 'flightNumber',
+          },
+          {
+            Header: 'Mission',
+            accessor: 'missionName',
+          },
+          {
+            Header: 'Details',
+            accessor: 'details',
+          },
+          {
+            Header: 'Launch Status',
+            accessor: 'launchStatus',
+          },
+          {
+            Header: 'Launch Date',
+            accessor: 'launchDate',
+          },
+          {
+            Header: 'Launch Year',
+            accessor: 'launchYr',
+          },
+          {
+            Header: 'Launch Site',
+            accessor: 'launchSite',
+          },
+        ]}
+      />
+    </Fragment>
+  );
+};
+
+
+const mapStateTpProps = state => ({
+  launches: state.launches.launches,
+});
+
+
+export default connect(mapStateTpProps, { getLaunches })(Launches);
